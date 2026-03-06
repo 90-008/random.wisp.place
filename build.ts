b9bbc24d6f81c68b4337c09dd2ecc18eaaa9e43d
dawn <90008@gaze.systems>
2026-03-06 13:23:16 +0300

    wisp deploy

--- build.ts
+++ build.ts
@@ -1,3 +1,5 @@
 const html = await Deno.readTextFile("redirect.html");
 const url = Deno.env.get("API_URL") ?? "/";
-await Deno.writeTextFile("redirect.out.html", html.replaceAll("__API_URL__", url));
+await Deno.remove("dist", { recursive: true }).catch(() => {});
+await Deno.mkdir("dist");
+await Deno.writeTextFile("dist/redirect.html", html.replaceAll("__API_URL__", url));
